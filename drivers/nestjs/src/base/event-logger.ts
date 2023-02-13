eca556645f2fb12da8caaab65254db5d38b7687e
hbinhng <binhmattroi@gmail.com>
2023-02-13 14:44:48 +0700

    Use client instance

--- drivers/nestjs/src/base/event-logger.ts
+++ drivers/nestjs/src/base/event-logger.ts
@@ -1,11 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 
 import { LogLevels } from '../data';
 import { EventLogBuilder } from './event-log-builder';
+import { Client } from 'pg';
 
 @Injectable()
 export class EventLogger {
+  public constructor(@Inject('CONNECTION') private readonly client: Client) {}
+
   public create(type: LogLevels): EventLogBuilder {
-    return new EventLogBuilder(null, type);
+    return new EventLogBuilder(this.client, type);
   }
 }
